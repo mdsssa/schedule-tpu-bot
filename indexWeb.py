cef8e03e5309f231207dba9a82dd24156208e900
Dissa <mdssa1337@gmail.com>
2025-11-04 22:26:32 +0700

    ver 1.0 fixed zombie processes bug

--- indexWeb.py
+++ indexWeb.py
@@ -61,7 +61,7 @@ def isBackPairs(index , element):
 def get_driver():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--window-size=1920,1080')
