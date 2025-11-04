22185eeefe5050bc254df0126e9e67f2cd0d7bb9
Dissa <mdssa1337@gmail.com>
2025-11-04 23:03:35 +0700

    ver 1.0 fixed zombie processes bug

--- indexWeb.py
+++ indexWeb.py
@@ -62,7 +62,7 @@ def get_driver():
     chrome_options = Options()
     # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument('--disable-dev-shm-usage')
     # chrome_options.add_argument('--no-zygote')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-crash-reporter')
