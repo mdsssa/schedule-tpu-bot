02842cad17a42cea34e721baf425645fd3583ea4
Dissa <mdssa1337@gmail.com>
2025-11-27 22:27:22 +0700

    ver 1.1

--- indexTelegram.py
+++ indexTelegram.py
@@ -79,6 +79,7 @@ def send_to_logger(ex , id = 0 , isntanexeption = False , justInfo = False):
         save_log(message, id)
     except Exception as e:
         save_log(ex, id)
+
 def makeMarkupWithLayout(layout:dict):
     try:
         markup = InlineKeyboardMarkup()
@@ -644,7 +645,7 @@ def distributionSide():
                             send_to_logger(e , user.id)
                         try:
                             bot.send_message(user , f'Расписание для {school} , {course} курс , группа {group}:\n\n{schedule[0]}' , reply_markup= markup)
-                            bot.send_message(user , f'У нас теперь есть канал в телеграме!\nТам выкладываются новости бота , а так же там вы можете оставлять свои идеи по улучшению проекта👨‍💻\n@scheduletpunews' , reply_markup= markup)
+                            # bot.send_message(user , f'У нас теперь есть канал в телеграме!\nТам выкладываются новости бота , а так же там вы можете оставлять свои идеи по улучшению проекта👨‍💻\n@scheduletpunews' , reply_markup= markup)
                         except Exception as e:
                             if str(e).lower() == 'A request to the Telegram API was unsuccessful. Error code: 403. Description: Forbidden: bot was blocked by the user'.lower():
                                 deleteUser(user)
