916b076e834c6427d40f02a89d4ac6b28f90bd53
Dissa <mdssa1337@gmail.com>
2025-12-15 17:13:36 +0700

    ver 1.1.1

--- indexWeb.py
+++ indexWeb.py
@@ -40,7 +40,7 @@ def get_pairs_without_deleted(id , data , fullWeek = False):
                     if True in [True if i == lecture_data[0] else False for i in types_]:
                         lecture = f'{lecture_data[1]} {lecture_data[0]}'
                     if lecture in deleted_lect:
-                        data[i][j] == 'Вы удалили данный предмет.'
+                        data[i][j] == 'Вы удалили данный предмет. '
         else:
             for j, element in enumerate(data):
                 lecture_data = element[1].split('\n')
