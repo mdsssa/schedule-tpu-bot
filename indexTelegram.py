5fa5582206ade821d056aa27b843912722c60705
Dissa <mdssa1337@gmail.com>
2025-12-15 11:29:42 +0700

    ver 1.1.1

--- indexTelegram.py
+++ indexTelegram.py
@@ -497,9 +497,11 @@ def telegramSide():
             elif data.startswith('del_l'):
                 markup = InlineKeyboardMarkup()
                 markup.add(InlineKeyboardButton("MENU", callback_data=f"adminMenu"))
-                add_deleted_lectures(chat_id , data.replace('del_l_' , ''))
-                markup.add(InlineKeyboardButton("Отмена" , callback_data=f'un{data.replace('d' , '')}'))
-                bot.send_message(chat_id , f"Предмет {data.replace('del_l_' , '')} удален , он больше не будет появляться в вашем расписании" , reply_markup=markup)
+                lect = users_lects[chat_id][int(data.replace('del_l_' , ''))]
+                add_deleted_lectures(chat_id , lect)
+
+                markup.add(InlineKeyboardButton("Отмена" , callback_data=f'un{data.replace('del_l' , '')}'))
+                bot.send_message(chat_id , f"Предмет {lect} удален , он больше не будет появляться в вашем расписании" , reply_markup=markup)
 
                 print(get_deleted_lectures(chat_id))
 
