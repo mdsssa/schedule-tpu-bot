70c33e70ab1d662040bedd953e2320ac2b75e866
Dissa <mdssa1337@gmail.com>
2026-03-13 19:52:28 +0700

    ver 1.1.1

--- indexTelegram.py
+++ indexTelegram.py
@@ -729,8 +729,8 @@ def telegramSide():
         bot.send_message(message.from_user.id , text=menuText , reply_markup= generateMenu(message.from_user.id))
     @bot.message_handler(commands= ['ping'])
     def pingHandler(message:telebot) -> None:
-        start_time = float(time.time())
-        bot.send_message(message.from_user.id , f'pong!\n{float(time.time()) - start_time}')
+        start_time = float(time.time()) * 1000
+        bot.send_message(message.from_user.id , f'pong!\n{float(time.time()) * 1000 - start_time}')
     @bot.message_handler(commands= daysOfWeek["rus"] + daysOfWeek["eng"])    
     def LastHandler(message) -> None:
         manageMessages(id=message.from_user.id, messageId=message.id - 1)
